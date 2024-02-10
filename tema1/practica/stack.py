85e38dfb4b87fe078102d05adb7537ead339d242
xMakuno <mamv2001@gmail.com>
2024-02-10 12:42:50 -0600

    👍add: a - [ds2] stack python

---
+++ tema1/practica/stack.py
@@ -0,0 +1,22 @@
+def parseOperation(thing, a, b):
+    if( thing == "+"):
+        return a + b
+    if( thing == "-"):
+        return a - b
+    if( thing == "*"):
+        return a * b
+
+
+if __name__ == "__main__":
+    operators = [ "+", "-", "*"]
+    array = [ a if a in operators else int(a) for a in input().split(" ")]
+    stack = []
+    for element in array:
+        if(type(element) is int):
+            stack.append(element)
+        else:
+            result = parseOperation(element, stack.pop(-2), stack.pop(-1))
+            stack.append(result)
+            
+            
+    print(stack[0])
