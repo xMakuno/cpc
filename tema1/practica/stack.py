5b7ebff553668c4a3b3a92c34511c5fac014c872
xMakuno <mamv2001@gmail.com>
2024-02-10 14:17:52 -0600

    👍add: [ds2] - stack debug comments

--- tema1/practica/stack.py
+++ tema1/practica/stack.py
@@ -10,12 +10,14 @@ def parseOperation(thing, a, b):
 if __name__ == "__main__":
     operators = [ "+", "-", "*"]
     array = [ a if a in operators else int(a) for a in input().split(" ")]
+    # print(array)
     stack = []
     for element in array:
         if(type(element) is int):
             stack.append(element)
         else:
             result = parseOperation(element, stack.pop(-2), stack.pop(-1))
+            # print(result)
             stack.append(result)
             
             
