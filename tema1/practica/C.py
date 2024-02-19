a57f5e4d9db5cb8ba732d94e39c473c95b2bd5df
xMakuno <mamv2001@gmail.com>
2024-02-18 21:47:36 -0600

    update: included deque collection into algorithm

--- tema1/practica/C.py
+++ tema1/practica/C.py
@@ -1,36 +1,41 @@
-def parsePush(operation, queue, val):
-    if operation == "pushLeft":
-        queue.insert(0, val)
-        print(f'Pushed in left: {val}')
-    if operation == "pushRight":
-        queue.append(val)
-        print(f'Pushed in right: {val}')
+from collections import deque
 
-def parsePop(operation, queue):
-    if operation == "popLeft":
-        print(f'Popped from left: {queue.pop(0)}')
-    if operation == "popRight":
-        print(f'Popped from left: {queue.pop(-1)}')
+def parsePush(deq, word, val):
+    if word == 'pushLeft':
+        deq.appendleft(val)
+        print(f'Pushed in left: {val}')
+    elif word == 'pushRight':
+        deq.append(val)
+        print(f'Pushed in right: {val}')
+    else:
+        print("cagaste push")
+
+def parsePop(deq, word):
+    if word == 'popLeft':
+        print(f'Popped from left: {deq.popleft()}')
+    elif  word == 'popRight':
+        print(f'Popped from left: {deq.pop()}')
+    else:
+        print("cagaste pop")
 
 if __name__ == "__main__":
-    caseNum = input()
-    caseNum = int(caseNum)
-    for i in range(caseNum):
-        size, commands = input().split(" ")
-        size = int(size)
-        commands = int(commands)
-        print(f'\nCase {i+1}:')
-        doubleQueue = []
-        for j in range(commands):
-            x = input().split(" ")
-            if len(x) == 2:
-                if len(doubleQueue) == size:
+    T = int(input())
+
+    for i in range(1, T+1):
+        print(f'Case {i}:')
+        n, m = [int(a) for a in input().split(" ")]
+        dq = deque([])
+        for i in range(0, m):
+            command = [a for a in input().split(" ")]
+            if command[0][1] == 'u': # if second char of the word is u == push else o == pop
+                if len(dq) >= n: # check if the deque is full
                     print("The queue is full")
                 else:
-                    parsePush(x[0], doubleQueue, int(x[1]))
-            else: # Pop command
-                # Cannot pop if empty
-                if len(doubleQueue) == 0:
+                    parsePush(dq, command[0], int(command[1]))
+            elif command[0][1] == 'o':
+                if len(dq) <= 0:
                     print("The queue is empty")
                 else:
-                    parsePop(x[0], doubleQueue)
+                    parsePop(dq, command[0])
+            else:
+                print("cagaste command")
