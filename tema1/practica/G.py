a69e247a102ababd37c964ff5fbb5f0d90740eab
xMakuno <mamv2001@gmail.com>
2024-03-22 13:17:13 -0600

    👍add: AC solution for problem B topic 1 Practice

---
+++ tema1/practica/G.py
@@ -0,0 +1,30 @@
+import sys
+ 
+def solve():
+    input = sys.stdin.readline # input is 10 times faster 
+    print = sys.stdout.write
+
+    n, k, q = map(int, input().split())
+    recipes = [0] * (200000 + 2)
+    res = []
+    
+    for _ in range(n):
+        l, r = map(int, input().split())
+        recipes[l] += 1
+        recipes[r+1] -= 1
+    
+    for i in range(1, len(recipes)):
+        recipes[i] += recipes[i-1]
+    
+    for i in range(1, len(recipes)):
+        recipes[i] = 1 if recipes[i] >= k else 0
+        recipes[i] += recipes[i-1]
+    
+    for _ in range(q):
+        a, b = map(int, input().split())
+        res.append((recipes[b] - recipes[a-1]))
+    
+    print('\n'.join(map(str, res)))
+
+if __name__ == "__main__":
+    solve()
