89922c05fac52fe17498e953273e19ec94e56238
xMakuno <mamv2001@gmail.com>
2024-03-30 13:10:23 -0600

    👍add: faster impl for problem A topic 2 contest

--- tema2/concurso/A.py
+++ tema2/concurso/A.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
         thing.append(temp)
         if temp == 0:
             break
-    for i in thing[::-1]:
-        print(i)
+    while thing:
+        print(thing.pop())
