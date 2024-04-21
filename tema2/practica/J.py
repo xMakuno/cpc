d42fdd0d49acf4f237407d2c16d23541fbf853b8
xMakuno <mamv2001@gmail.com>
2024-04-20 22:59:21 -0600

    👍refactor: problem J code topic 2 practice 2

--- tema2/practica/J.py
+++ tema2/practica/J.py
@@ -1,14 +1,26 @@
+def searchInsert(nums, target):
+    hi = len(nums) - 1
+    lo = 0
+    while lo <= hi:
+        mid = lo + (hi - lo)//2
+        if nums[mid][0] == target: return mid
+        if nums[mid][0] > target: hi = mid - 1
+        else: lo = mid + 1
+    return lo
+
 if __name__ == "__main__":
-    n, m = [int(x) for x in input().split(" ")]
-    a = [int(x) for x in input().split(" ")]
-    b = [int(x) for x in input().split(" ")]
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    a = [[x, 1] for x in a]
+    memo = {}
+    for thing in a:
+        if thing not in memo:
+            memo.update({thing:1})
+        else:
+            memo[thing] += 1
+    b = list(map(int, input().split()))
     a.sort()  
     ans = []
     for j in b:
-        for i in range(n-1, -1,-1):
-            if a[i] <= j:
-                ans.append(i+1)
-                break
-            if i == 0 and a[i] >= j:
-                ans.append(0)
+        ans.append(searchInsert(a, j))
     print(*ans)
