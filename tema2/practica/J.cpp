ac3f82fdcda459458762cf655eea4abddfe5b951
xMakuno <mamv2001@gmail.com>
2024-04-20 22:58:54 -0600

    👍add: mario implementation for J topic 2 practice

--- tema2/practica/J.cpp
+++ tema2/practica/J.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 typedef long long ll;
 
-void solve(){
+void gabo(){
     ll n, m;
     cin >> n >> m;
     ll a[n];
@@ -30,7 +30,31 @@ void solve(){
     }
 }
 
+void solve(){
+    int n, m;
+    cin >> n >> m;
+    int a[n];
+    for(int i = 0; i < n; ++i){
+        cin >> a[i];
+    }
+    sort(a, a+n);
+    for(int i = 0; i < m; ++i){
+        int b;
+        cin >> b;
+        long long lo = 0, hi = n -1;
+        while(lo <= hi){
+            int mid = lo + ((hi - lo )/ 2);
+            if(a[mid] <= b){
+                lo = mid;
+            }else{
+                hi = mid - 1;
+            }
+        }
+        cout << (lo != 0 ? lo + 1 : (a[lo] > b ? 0 : 1)) << " \n"[i == m - 1];
+    }
+}
+
 int main(){
-    solve();
+    gabo();
     return 0;
 }
