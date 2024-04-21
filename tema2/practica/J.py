a2db15f058cfa3fc14a108ac2a10c94bd21fda80
xMakuno <mamv2001@gmail.com>
2024-04-21 17:05:10 -0600

    👍add: ac for J topic 2 practice

--- tema2/practica/J.py
+++ tema2/practica/J.py
@@ -1,26 +1,24 @@
 def searchInsert(nums, target):
     hi = len(nums) - 1
     lo = 0
-    while lo <= hi:
-        mid = lo + (hi - lo)//2
-        if nums[mid][0] == target: return mid
-        if nums[mid][0] > target: hi = mid - 1
-        else: lo = mid + 1
+    while lo < hi:
+        mid = (lo + hi + 1)//2
+        print(f'hi: {hi} lo: {lo} mid: {mid}')
+        if nums[mid] <= target: lo = mid
+        else: hi = mid - 1
     return lo
 
 if __name__ == "__main__":
     n, m = map(int, input().split())
     a = list(map(int, input().split()))
-    a = [[x, 1] for x in a]
-    memo = {}
-    for thing in a:
-        if thing not in memo:
-            memo.update({thing:1})
-        else:
-            memo[thing] += 1
     b = list(map(int, input().split()))
     a.sort()  
     ans = []
     for j in b:
-        ans.append(searchInsert(a, j))
+        temp = searchInsert(a, j)
+        if temp != 0: temp+=1
+        else:
+            if a[temp] > j: temp = 0
+            else: temp =1
+        ans.append(temp)
     print(*ans)
