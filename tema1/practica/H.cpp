963ff67731464c6489760caff5e250c890b23434
xMakuno <mamv2001@gmail.com>
2024-04-20 22:57:52 -0600

    👍add: tle topic 2 practice for H

---
+++ tema1/practica/H.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, q;
+    cin >> n >> q;
+    int array[n];
+    deque<int> fuck;
+    for(int i = 0; i < n; ++i){
+        cin >> array[i];
+    }
+    long long m;
+    for(int i = 0; i < n; ++i){
+
+    }
+    return 0;
+}
---
+++ tema1/practica/H.py
@@ -0,0 +1,27 @@
+from collections import deque 
+from copy import copy
+def solve():
+    n, q = map(int, input().split())
+    nums = deque(map(int, input().split()))
+    if(q == 0):
+        return
+    # print(nums)
+    for i in range(q):
+        t = int(input())
+        temp = copy(nums)
+        while t:
+            t-=1
+            a = temp.popleft()
+            b = temp.popleft()
+            
+            if a > b:
+                temp.appendleft(a)
+                temp.append(b)
+            else:
+                temp.appendleft(b)
+                temp.append(a)
+            # print(nums)
+        print(f'{a} {b}')
+    
+if __name__ == "__main__":
+    solve()
