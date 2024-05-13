6aaf200ff7d3a9a9a479f39263ec54f61cbf5c59
xMakuno <mamv2001@gmail.com>
2024-05-12 19:38:51 -0600

    👎add: WA for K topic 2 practice

--- tema2/practica/K.cpp
+++ tema2/practica/K.cpp
@@ -3,62 +3,39 @@ using namespace std;
 
 typedef long long ll;
 
-struct part{
-    ll a, b, i;
+struct person{
+    int pos;
+    float p;
 };
-
-bool comp(part a, part b){
-    ll aD = a.a + a.b, bD = b.a + b.b;
-    if(aD == bD){
-        if(a.a > b.a){
+bool personComp(person a, person b){
+    if(a.p < b.p){
+        return true;
+    }else if(a.p > b.p){
+        return false;
+    }else{
+        if(a.pos < b.pos){
             return true;
-        }
-        else if (a.a == b.a){
-            if(a.i < b.i){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        else{
-            return false;
-        }
-    }
-    else{
-        ll cD = lcm(aD, bD);
-        ll aX = cD / aD, bX = cD / bD;
-        ll aT = a.a * aX, bT = b.a * bX;
-        if(aT > bT){
-            return true;
-        }
-        else if (aT == bT){
-            if(a.i < b.i){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        else{
+        }else{
             return false;
         }
     }
 }
-
 void solve(){
-    int n;
+    int n, a, b;
     cin >> n;
-    part sV[n];
-    for(int i = 0; i < n; ++i){
-        int a, b;
-        part t;
-        cin >> sV[i].a >> sV[i].b;
-        sV[i].i = i+1;
+    person t[n];
+    for(int i=0; i < n; ++i){
+        cin >> a >> b;
+        t[i].pos = i+1; 
+        if(a != 0){
+            t[i].p = 1 / 1 + (b/a);
+        }else{
+            t[i].p = 0;
+        }
     }
-    sort(sV, sV + n, comp);
-    for(int i = 0; i < n; ++i){
-        cout << sV[i].i << " \n"[i == n];
+    sort(t, t + n, personComp);
+    for(int i=0; i < n; ++i){
+        cout << t[i].pos << " \n"[i == n -1];
     }
 }
 
