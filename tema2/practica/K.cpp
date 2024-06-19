fe784feaa8df1b674ec37d4610e8cd3d654a525b
xMakuno <mamv2001@gmail.com>
2024-06-19 14:00:52 -0600

    👎add: WA for K topic 2 practice

--- tema2/practica/K.cpp
+++ tema2/practica/K.cpp
@@ -6,27 +6,52 @@ typedef long long ll;
 struct person{
     int pos;
     float p;
+    int a,b;
 };
-bool personComp(person a, person b){
-    if(a.p < b.p){
+
+bool posCheck(int a, int b){
+    if(a < b){
         return true;
-    }else if(a.p > b.p){
-        return false;
     }else{
-        if(a.pos < b.pos){
-            return true;
-        }else{
-            return false;
-        }
+        return false;
     }
 }
-void solve(){
+    /* 
+    ai/bi = left person
+    aj/bj = right person
+
+    left has bigger fraction -> lower prob -> false
+    left has smaller fraction -> higher prob -> true
+    equal -> check position
+    ai = aj
+    */
+
+
+bool personComp(person i, person j){
+    if(i.p < j.p){
+        return true;
+    }else if(i.p > j.p){
+        return false;
+    }
+    long long c = i.b*j.a;
+    long long d = j.b*i.a;
+    if(c < d){
+        return true;
+    }else if(c > d){
+        return false;
+    }else{
+        return posCheck(i.pos, j.pos);
+    }
+}
+void solve(){ // WA 9/23
     int n, a, b;
     cin >> n;
     person t[n];
     for(int i=0; i < n; ++i){
         cin >> a >> b;
         t[i].pos = i+1; 
+        t[i].a = a;
+        t[i].b = b;
         if(a != 0){
             t[i].p = 1 / 1 + (b/a);
         }else{
