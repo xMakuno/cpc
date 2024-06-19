bc43fecb18b133309fe0c7269c62cfd6ef139229
xMakuno <mamv2001@gmail.com>
2024-06-19 14:58:43 -0600

    👍add: H.cpp yes

--- tema1/practica/H.cpp
+++ tema1/practica/H.cpp
@@ -1,6 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+
+typedef long long ll;
+
+void solve(){
+    int n, q, maxValue = -1;
+    cin >> n >> q;
+    deque<int> nums(n,0);
+    for(int i = 0; i < n; ++i){
+        cin >> nums[i];
+        maxValue = max(maxValue, nums[i]); // Max value calculation for the entire array
+    }
+    map<int,pair<int, int>> answer;
+    int index =0;
+    while(true){
+        // cout << "que\n";
+        int a = nums.front(); nums.pop_front();
+        int b = nums.front(); nums.pop_front();
+        if(a == maxValue){ // if A is max, the array will just cycle through b's from now on.
+            nums.push_front(b); // [ b, x1, x2,x3, ...]
+            nums.push_front(a); // [ a, b, x1, x2, x3, ...]
+            break;
+        }
+        ++index;
+        answer[index] = {a, b};
+        if(b > a){
+            swap(a,b);
+        }
+        nums.push_front(a);
+        nums.push_back(b);
+    }
+    int residue[n];
+    for(int i = 0; i < n; ++i){
+        residue[i] = nums.front(); nums.pop_front();
+    }
+    for(int i = 0; i < q; ++i){
+        ll qi;
+        cin >> qi;
+        if(qi <= index){
+            cout << answer[qi].first << " " << answer[qi].second << '\n';
+        }else{
+            cout << maxValue << " " << residue[((qi - (index + 1))% (n-1)) + 1] << '\n';
+        }
+    }
+
+}
+
+void bruteForce(){
     int n, q;
     cin >> n >> q;
     deque<int> nums(n,0);
@@ -27,6 +73,9 @@ int main(){
         }
         cout << a << " " << b << '\n';
     }
+}
 
+int main(){
+    solve();
     return 0;
 }
