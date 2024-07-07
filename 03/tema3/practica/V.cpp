c4dffa35d1abbd3ab4685630d399c0241e1f3f92
xMakuno <mamv2001@gmail.com>
2024-07-07 10:08:40 -0600

    🎨 update: reformat document problem V

--- 03/tema3/practica/V.cpp
+++ 03/tema3/practica/V.cpp
@@ -3,20 +3,17 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 int main(){
-    int n;
-    cin >> n;
+    int n;  cin >> n;
     map<int, int> mp;
-    int q; cin >> q;
+    int q;  cin >> q;
     while(q--){
-        int option;
-        cin >> option;
+        int option; cin >> option;
         if(option == 1){
             int k;  cin >> k;
             mp.insert({k,-1});
         }else{
             int y;  cin >> y;
             auto it = mp.lower_bound(y);
-            
             if(it == mp.end()){
                 cout << "-1\n";
             }else{
