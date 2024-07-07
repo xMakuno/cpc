a4b682f1e7b470e9d94edf032080f02d86b0d029
xMakuno <mamv2001@gmail.com>
2024-07-06 22:27:29 -0600

    🏗️update: project structure

---
+++ 01/practice/B.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    int n, t, segs = 0;
+    cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n; ++i) cin >> v[i];
+    for(int i = 1; i < n; ++i){
+
+    }
+    return 0;
+}
