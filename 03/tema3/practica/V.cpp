6ddefb05a02adf55dd9b1e405ae0ea60de1f97d5
xMakuno <mamv2001@gmail.com>
2024-07-07 11:30:48 -0600

    👍add: REAL REAL AC for V

--- 03/tema3/practica/V.cpp
+++ 03/tema3/practica/V.cpp
@@ -1,23 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> pii;
 int main(){
+    ios::sync_with_stdio(false); // literally this makes it fast enough for AC
+    cin.tie(nullptr); // same with this line
     int n;  cin >> n;
-    map<int, int> mp;
+    set<int> mp;
     int q;  cin >> q;
     while(q--){
         int option; cin >> option;
         if(option == 1){
             int k;  cin >> k;
-            mp.insert({k,-1});
+            mp.insert(k);
         }else{
             int y;  cin >> y;
             auto it = mp.lower_bound(y);
             if(it == mp.end()){
                 cout << "-1\n";
             }else{
-                cout << (*it).first << '\n'; 
+                cout << (*it) << '\n'; 
             }
         }
     }
