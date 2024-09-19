4f5d00fba86abb88344af398a3997cc76bbc6c5b
xMakuno <mamv2001@gmail.com>
2024-09-19 08:56:11 -0600

    👍add: AC for AA practice

--- 03/solo/tema4/practica/AA.cpp
+++ 03/solo/tema4/practica/AA.cpp
@@ -1,16 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; ++i){
+        cin >> nums[i];
+    }
+    sort(nums.begin(),nums.end());
+    if(nums[0] != nums[1]){
+        cout << "YES\n";
+        return;
+    }
+    for(int i = 1; i < n; ++i){
+        if(nums[i] % nums[0] != 0){
+            cout << "YES\n";
+            return;
+        }
+    }
+    cout << "NO\n";
+}
+
 int main(){
     int T = 1;
     cin >> T;
     while(T--){
-        int n;
-        cin >> n;
-        vector<int> nums(n);
-        for(int i = 0; i < n; ++i){
-            cin >> nums[i];
-        }
+        solve();
     }
     return 0;
 }
