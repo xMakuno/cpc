5338209e268574268eda2d1d1f8a58ea77b934ec
xMakuno <mamv2001@gmail.com>
2024-09-18 22:52:43 -0600

    👍add: base code for AA practice

---
+++ 03/solo/tema4/practica/AA.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    int T = 1;
+    cin >> T;
+    while(T--){
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        for(int i = 0; i < n; ++i){
+            cin >> nums[i];
+        }
+    }
+    return 0;
+}
