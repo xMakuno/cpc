5dc30640143bfc70374c3cbb88577678d575ea84
xMakuno <mamv2001@gmail.com>
2024-09-30 23:28:23 -0600

    👍add: base code for AI practice problem

---
+++ 03/solo/tema5/AI.cpp
@@ -0,0 +1,16 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    /*
+    freopen("planting.in", "r", stdin);
+    freopen("planting.out", "w", stdout);
+    */
+    int T = 1;
+    cin >> T;
+    while(T--){
+
+    }
+    return 0;
+}
