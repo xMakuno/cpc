32c265c6e5dc25a7786a5e5af0eacff34fee1b6c
xMakuno <mamv2001@gmail.com>
2024-10-02 14:51:42 -0600

    👎add:  WA for C team contest 1

---
+++ 03/teams/01/E.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+//WA needs to be DSU
+
+void dfs(vector<bool> &visited, vector<vector<int>> &al, int u){
+    visited[u] = true;
+    // cout << "looking: " << u << '\n'; 
+    for(auto &v : al[u]){
+        if(visited[v] == false){
+            dfs(visited, al, v);
+        }
+    }
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<bool> visited(n+1,false);
+    vector<vector<int>> al(n+1);
+    for(int i = 0; i < n; ++i){
+        int u, v;
+        cin >> u >> v;
+        al[u].push_back(v);
+        al[v].push_back(u);
+    }
+    int ans = 0;
+    for(int i = 1; i <= n; ++i){
+        if(visited[i] == false){
+            ans++;
+            dfs(visited, al, i);
+        }
+    }
+    cout << ans - 1 << '\n';
+    return 0;
+}
