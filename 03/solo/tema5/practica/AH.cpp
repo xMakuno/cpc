5e01bed4e3bd8f8ee2e78713fe205b261c330695
xMakuno <mamv2001@gmail.com>
2024-10-05 20:39:34 -0600

    👍add: AC for AH practice GT

--- 03/solo/tema5/practica/AH.cpp
+++ 03/solo/tema5/practica/AH.cpp
@@ -2,26 +2,39 @@
 using namespace std;
 typedef long long ll;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     int T;
     cin >> T;
     while(T--){
         int n, m;
         cin >> n >> m;
-        vector<int> edges(m,0);
+        vector<vector<int>> edges(m);
         int temp;
         for(int i = 0; i < n; ++i){
             for(int j = 0; j < m; ++j){
                 cin >> temp;
                 if(temp){
-                    edges[j]++;
+                    edges[j].push_back(i);
                 }
             }
         }
         bool good = true;
         for(int i = 0; i < m; ++i){
-            if(edges[i] > 2){
+            if(edges[i].size() != 2){
                 good = false;
                 break;
+            }
+        }
+
+        if(good){
+            // cout << "Third test doesnt reach here\n";
+            set<pair<int, int>> complete; // size should equal m
+            for(int i = 0; i < m; ++i){
+                complete.insert({edges[i][0], edges[i][1]});
+            }
+            if(complete.size() != m){
+                good = false;
             }
         }
         cout << (good? "Yes\n": "No\n");
